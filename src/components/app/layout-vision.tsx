5c969cc349ce749e95a4876712c03e21cb042d5e
Débora Castro <debora@vert-capital.com>
2024-09-16 11:12:00 -0300

    fix layout vision

--- src/components/app/layout-vision.tsx
+++ src/components/app/layout-vision.tsx
@@ -1,11 +1,11 @@
-export default function LayoutVision() {
+export const LayoutVision = () => {
   return (
     <div className="layout-vision">
       <div className="layout-vision__container">
         <div className="layout-vision__content">
           <h1 className="layout-vision__title">Vision</h1>
           <p className="layout-vision__text">
-            Our vision is to create a better everyday life for many people. We believe everyone deserves to have a home they love, and we are committed to helping them find it.
+            Our vision is to create a better everyday life for many people.
           </p>
         </div>
       </div>
